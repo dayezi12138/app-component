48255e85c49fa6387631f50724e84d4151296f25
dayezi <yunzewang@163.com>
2019-10-12 11:02:20 +0800

    提交同步

---
+++ IMKit/src/main/java/io/rong/imkit/MainActivity.java
@@ -0,0 +1,10 @@
+package io.rong.imkit;
+
+import android.app.Activity;
+import android.support.v4.app.FragmentManager;
+
+/**
+ * Created by Bob on 15/7/7.
+ */
+public class MainActivity extends Activity {
+}
